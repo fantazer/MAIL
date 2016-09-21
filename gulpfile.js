28847cd4fef82497e54abbbbc6b6fcc309837dcf
Your Name <you@example.com>
2016-09-21 16:00:12 +0300

    add live server

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,46 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     inky = require('inky'),
     inlineCss = require('gulp-inline-css');
-
+var browserSync = require('browser-sync').create();
 
 //STYLES
 gulp.task('styles', function () {
   return gulp.src('./scss/styles.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .on('end', browserSync.reload);
 });
 
 //CONVERTE INKY
 gulp.task('inky', function() {
   return gulp.src('./templates/**/*.html')
     .pipe(inky())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .on('end', browserSync.reload);
 });
 
 //INLINE CSS
-gulp.task('inline', function () {
+gulp.task('build', function () {
   return gulp.src('./dist/*.html')
         .pipe(inlineCss())
         .pipe(gulp.dest('./dist/inlined'));
 });
 
+//LiveReload
+gulp.task('serve', function () {
+    browserSync.init({
+        notify: false,
+        reloadDelay: 300,
+        server: {
+            baseDir: "./dist/",
+        }
+    });
+    browserSync.watch(["./scss/styles.scss" ,"./templates/**/*.html"]).on("change", browserSync.reload);
+});
 
 //WATCH
-gulp.task('default',function() {
-    gulp.watch('./scss/**/*.scss',['styles']);
-    gulp.watch('./templates/**/*.html',['inky']);
-});
+gulp.task('see',function(){
+        gulp.watch('./scss/**/*.scss',['styles']);
+      gulp.watch('./templates/**/*.html',['inky']);
+})
+gulp.task('default',['see','serve'] );
